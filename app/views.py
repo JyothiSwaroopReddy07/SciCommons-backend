c2368ecc3bcb59cfe520e4cfdc9531b8fd7765b5
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-07-06 22:05:32 +0530

    added unsubscribed

--- app/views.py
+++ app/views.py
@@ -270,6 +270,13 @@ class CommunityViewset(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         
         return Response(data={"success": "subscribed successfully"})
+    
+    @action(methods=['DELETE'], detail=False, url_path='unsubscribe',permission_classes=[CommunityPermission])
+    def subscribe(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        return Response(data={"success": "unsubscribed successfully"})
 
     @action(methods=['DELETE'], detail=False, url_path='unsubscribe',permission_classes=[CommunityPermission])
     def unsubscribe(self, request):
