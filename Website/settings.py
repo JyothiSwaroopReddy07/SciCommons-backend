fbbf557d7ffa0775f6e99605f5fb190aea8063d4
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-09-09 23:46:50 +0530

    added serializer

--- Website/settings.py
+++ Website/settings.py
@@ -67,7 +67,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 20
+    "PAGE_SIZE": 50
 }
 
 MIDDLEWARE = [
