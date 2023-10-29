549686cbe60c9d4bbec9b8b3d63e61d3e3f5e6d1
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-10-29 23:02:36 +0530

    Update settings.py

--- Website/settings.py
+++ Website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$$n^0_i1iuqd91m+dc_=)kj8o)q$o#)3m5#hl+h3n=vt-#mkz)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["scicommons-backend.onrender.com","127.0.0.1"]
 
